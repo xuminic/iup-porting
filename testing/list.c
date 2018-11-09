14ef2d08ac8064ee66727894ea9a7b2a1a6c6671
Andy Xuming <xuming@users.sf.net>
2018-11-09 16:31:48 +1100

    A demo of the failure of SCROLLBAR attribution inside the IupList control. The SCROLLBAR doesn't work even in the simplest codes.

--- testing/list.c
+++ testing/list.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <iup.h>
 
+//#define CFG_RESIZE_EVENT
+//#define CFG_INITIAL_SIZE	"800x600"
+
+
 static char	*setlist[] = {
 	"IUP is a multi-platform toolkit for building graphical user interfaces.",
 	"It offers a simple API in three basic languages: C, Lua and LED.",
@@ -12,42 +16,68 @@ static char	*setlist[] = {
 	"due to the fact that it uses native interface elements.",
 	"fast learning by the user, due to the simplicity of its API.",
 	"This work was developed at Tecgraf/PUC-Rio by means of the partnership with PETROBRAS/CENPES.",
+	"Creates an interface element that displays a list of items.",
+	"The list can be visible or can be dropped down. It also can have an edit box for text input.",
+	"So it is a 4 in 1 element.",
+	"In native systems the dropped down case is called Combo Box.",
+	"The values can be any text. Items before \"1\" are ignored.",
+	"Before map the first item with a NULL is considered the end of the list and items can be set in any order.",
+	"After map, there are a few rules:",
+	"if \"1\" is set to NULL, all items are removed.",
+	"if \"id\" is set to NULL, all items after id are removed.",
+	"if \"id\" is between the first and the last item, the current idth item is replaced.",
+	"The effect is the same as removing the old item and inserting a new one at the old position.",
+	"if \"id+1\" is set then it is appended after the last item.",
+	"Items after \"id+1\" are ignored. (since 3.0)",
 	NULL
 };
 
-void SplitTest(void)
+static	Ihandle	*dlgmain, *ih_list;
+
+#ifdef	CFG_SCROLLBOX
+static	Ihandle	*sbox;
+#endif
+
+#ifdef	CFG_RESIZE_EVENT
+static int event_resize(Ihandle *ih, int width, int height)
 {
-  Ihandle *dlg, *bt, *split, *ml, *vbox;
-  int	i;
-
-  bt = IupList(NULL);
-  IupSetAttribute(bt, "EXPAND", "YES");
-  IupSetAttribute(bt, "MULTIPLE", "YES");
-  IupSetAttribute(bt, "SCROLLBAR", "YES");
-  IupSetAttribute(bt, "ALIGNMENT", "ARIGHT");
-  IupSetAttribute(bt, "CANFOCUS", "YES");
-  for (i = 0; setlist[i]; i++) {
-    IupSetStrAttributeId(bt, "", i+1, setlist[i]);
-  }
-
-  dlg = IupDialog(bt);
-  IupSetAttribute(dlg, "TITLE", "IupList Example");
-  IupSetAttribute(dlg, "RASTERSIZE", "800");
-
-  IupShow(dlg);
-}
-
-#ifndef BIG_TEST
-int main(int argc, char* argv[])
-{
-  IupOpen(&argc, &argv);
-
-  SplitTest();
-
-  IupMainLoop();
-
-  IupClose();
-
-  return 0;
+	printf("event_resize: %dx%d\n", width, height);
+	return IUP_DEFAULT;
 }
 #endif
+
+static void ListTest(void)
+{
+	int	i;
+
+	ih_list = IupList(NULL);
+	IupSetAttribute(ih_list, "EXPAND", "YES");
+	IupSetAttribute(ih_list, "MULTIPLE", "YES");
+	IupSetAttribute(ih_list, "SCROLLBAR", "YES");
+	IupSetAttribute(ih_list, "ALIGNMENT", "ARIGHT");
+	IupSetAttribute(ih_list, "CANFOCUS", "YES");
+	IupSetAttribute(ih_list, "AUTOHIDE", "NO");
+	for (i = 0; setlist[i]; i++) {
+		IupSetStrAttributeId(ih_list, "", i+1, setlist[i]);
+	}
+
+	dlgmain = IupDialog(ih_list);
+	IupSetAttribute(dlgmain, "TITLE", "IupList Example");
+#ifdef	CFG_INITIAL_SIZE
+	IupSetAttribute(dlgmain, "RASTERSIZE", CFG_INITIAL_SIZE);
+#endif
+#ifdef	CFG_RESIZE_EVENT
+	IupSetCallback(dlgmain, "RESIZE_CB", (Icallback) event_resize);
+#endif
+
+	IupShow(dlgmain);
+}
+
+int main(int argc, char **argv)
+{
+	IupOpen(&argc, &argv);
+	ListTest();
+	IupMainLoop();
+	IupClose();
+	return 0;
+}
